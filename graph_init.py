2b81111b5413c2d91665901e365e146c16d40827
user <982088729@qq.com>
2021-12-19 22:36:17 +0800

    [refactor] update v1

--- graph_init.py
+++ graph_init.py
@@ -2,30 +2,31 @@
 import argparse
 from utils import *
 from Graph_generate.lastfm_data_process import LastFmDataset
-from Graph_generate.lastfm_small_data_process import LastFmSmallDataset
+from Graph_generate.lastfm_star_data_process import LastFmStarDataset
 from Graph_generate.lastfm_graph import LastFmGraph
 from Graph_generate.yelp_data_process import YelpDataset
 from Graph_generate.yelp_graph import YelpGraph
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data_name', type=str, default=YELP, help='One of {LAST_FM, LAST_FM_SMALL, YELP, YELP_SMALL}.')
+    parser.add_argument('--data_name', type=str, default=YELP, choices=[LAST_FM, LAST_FM_STAR, YELP, YELP_STAR],
+                        help='One of {LAST_FM, LAST_FM_STAR, YELP, YELP_STAR}.')
     args = parser.parse_args()
     DatasetDict = {
         LAST_FM: LastFmDataset,
-        LAST_FM_SMALL: LastFmSmallDataset,
+        LAST_FM_STAR: LastFmStarDataset,
         YELP: YelpDataset,
-        YELP_SMALL: YelpDataset
+        YELP_STAR: YelpDataset
     }
     GraphDict = {
         LAST_FM: LastFmGraph,
-        LAST_FM_SMALL: LastFmGraph,
+        LAST_FM_STAR: LastFmGraph,
         YELP: YelpGraph,
-        YELP_SMALL: YelpGraph
+        YELP_STAR: YelpGraph
     }
 
     # Create 'data_name' instance for data_name.
-    print('Load', args.data_name, 'data_name from file...')
+    print('Load', args.data_name, 'from file...')
     print(TMP_DIR[args.data_name])
     if not os.path.isdir(TMP_DIR[args.data_name]):
         os.makedirs(TMP_DIR[args.data_name])
@@ -33,7 +34,7 @@ def main():
     save_dataset(args.data_name, dataset)
     print('Save', args.data_name, 'dataset successfully!')
 
-    # Generate graph instance for data_name
+    # Generate graph instance for 'data_name'
     print('Create', args.data_name, 'graph from data_name...')
     dataset = load_dataset(args.data_name)
     kg = GraphDict[args.data_name](dataset)
