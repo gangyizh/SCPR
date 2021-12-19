f2dacc77e8ba2a617bc0fc2dd2e2b12631771572
user <982088729@qq.com>
2021-12-19 22:20:33 +0800

    [feat] update v1

--- RL/utils.py
+++ RL/utils.py
@@ -4,76 +4,77 @@ import random
 import torch
 import os
 import sys
-# from knowledge_graph import KnowledgeGraph
-# from data_process import LastFmDataset
-# from KG_data_generate.lastfm_small_data_process import LastFmSmallDataset
-# from KG_data_generate.lastfm_knowledge_graph import KnowledgeGraph
-#Dataset names
-LAST_FM = 'lastfm'
-LAST_FM_SMALL = 'lastfm_small'
-YELP = 'yelp'
-YELP_SMALL = 'yelp_small'
+
+LAST_FM = 'LAST_FM'
+LAST_FM_STAR = 'LAST_FM_STAR'
+YELP = 'YELP'
+YELP_STAR = 'YELP_STAR'
 
 DATA_DIR = {
     LAST_FM: './data/lastfm',
     YELP: './data/yelp',
-    LAST_FM_SMALL: './data/lastfm_small',
-    YELP_SMALL: './data/yelp',
+    LAST_FM_STAR: './data/lastfm_star',
+    YELP_STAR: './data/yelp',
 }
 TMP_DIR = {
     LAST_FM: './tmp/last_fm',
     YELP: './tmp/yelp',
-    LAST_FM_SMALL: './tmp/last_fm_small',
-    YELP_SMALL: './tmp/yelp',
+    LAST_FM_STAR: './tmp/last_fm_star',
+    YELP_STAR: './tmp/yelp',
 }
+
+
 def cuda_(var):
     return var.cuda() if torch.cuda.is_available() else var
+
+def cprint(words : str):
+    print(f"\033[0;30;43m{words}\033[0m")
 def save_dataset(dataset, dataset_obj):
     dataset_file = TMP_DIR[dataset] + '/dataset.pkl'
     with open(dataset_file, 'wb') as f:
         pickle.dump(dataset_obj, f)
+
 
 def load_dataset(dataset):
     dataset_file = TMP_DIR[dataset] + '/dataset.pkl'
     dataset_obj = pickle.load(open(dataset_file, 'rb'))
     return dataset_obj
 
+
 def save_kg(dataset, kg):
     kg_file = TMP_DIR[dataset] + '/kg.pkl'
     pickle.dump(kg, open(kg_file, 'wb'))
+
 
 def load_kg(dataset):
     kg_file = TMP_DIR[dataset] + '/kg.pkl'
     kg = pickle.load(open(kg_file, 'rb'))
     return kg
 
-def save_sample_dict(dataset, sample_dict, mode='train'):
-    sample_file = TMP_DIR[dataset] + '/FM-sample-data/{}-sample-dict.pickle'.format(mode)
-    if not os.path.isdir(TMP_DIR[dataset] + '/FM-sample-data/'):
-        os.makedirs(TMP_DIR[dataset] + '/FM-sample-data/')
-    pickle.dump(sample_dict, open(sample_file, 'wb'))
-
-def load_sample_dict(dataset, mode='train'):
-    sample_file = TMP_DIR[dataset] + '/FM-sample-data/{}-sample-dict.pickle'.format(mode)
-    sample_dict = pickle.load(open(sample_file, 'rb'))
-    return sample_dict
 
 def save_fm_sample(dataset, sample_data, mode):
-    sample_file = TMP_DIR[dataset] + '/sample_fm_data_{}.pk1'.format(mode)
+    sample_file = DATA_DIR[dataset] + '/FM_sample_data/sample_fm_data_{}.pkl'.format(mode)
     print('save fm_{}_data in {}'.format(mode, sample_file))
     pickle.dump(sample_data, open(sample_file, 'wb'))
 
-def load_fm_sample(dataset, mode):
-    sample_file = TMP_DIR[dataset] + '/sample_fm_data_{}.pk1'.format(mode)
+
+def load_fm_sample(dataset, mode, epoch=0):
+    if mode == 'train':
+        sample_file = DATA_DIR[dataset] + '/FM_sample_data/sample_fm_data_{}-{}.pkl'.format(mode, epoch)
+
+    if mode == 'valid':
+        sample_file = DATA_DIR[dataset] + '/FM_sample_data/sample_fm_data_{}.pkl'.format(mode)
     with open(sample_file, 'rb') as f:
         sample_data = pickle.load(f)
     return sample_data
+
 
 def load_fm_model(dataset, model, filename, epoch):
     model_file = TMP_DIR[dataset] + '/FM-model-merge/' + filename + '-epoch-{}.pt'.format(epoch)
     model_dict = torch.load(model_file)
     print('Model load at {}'.format(model_file))
     return model_dict
+
 
 def save_fm_model(dataset, model, filename, epoch):
     model_file = TMP_DIR[dataset] + '/FM-model-merge/' + filename + '-epoch-{}.pt'.format(epoch)
@@ -89,6 +90,7 @@ def load_embed(dataset, epoch):
         embeds = pickle.load(f)
         print('FM Epoch：{} Embedding load successfully!'.format(epoch))
         return embeds
+
 
 def save_embed(dataset, embeds, epoch):
     path = TMP_DIR[dataset] + '/FM-model-embeds/' + 'embeds-epoch-{}.pkl'.format(epoch)
@@ -107,6 +109,7 @@ def save_fm_model_log(dataset, filename, epoch, epoch_loss, epoch_loss_2, train_
         f.write('Starting {} epoch\n'.format(epoch))
         f.write('training loss 1: {}\n'.format(epoch_loss / train_len))
         f.write('training loss 2: {}\n'.format(epoch_loss_2 / train_len))
+        f.write('=============================\n')
         # f.write('1000 loss: {}\n'.format(loss_1000))
 
 
@@ -114,23 +117,9 @@ def save_fm_sample_log(dataset, log_data, head_name, mode='train'):
     sample_file = TMP_DIR[dataset] + '/sample_fm_{}_log.txt'.format(mode)
     with open(sample_file, mode='w+', encoding='utf-8') as f:
         for name in head_name:
-            f.write(name+'\t')
+            f.write(name + '\t')
         f.write('\n')
         np.savetxt(f, log_data, fmt='%s', delimiter='\t')
-
-
-
-def load_rl_data(dataset, mode):
-    path = TMP_DIR[dataset] + '/rl_data_{}'.format(mode) + '.txt'
-    with open(path, mode='r+', encoding='utf-8') as f:
-        data = np.loadtxt(path, dtype=np.int64, delimiter=' ', comments='#')
-        print('load rl {} data successfully, size is {}'.format(mode, data.shape[0]))
-        return data
-
-def save_rl_data(dataset, data, head_name, mode):
-    path = TMP_DIR[dataset] + '/rl_data_{}'.format(mode) + '.txt'
-    with open(path, mode='w+', encoding='utf-8') as f:
-        np.savetxt(path, data, fmt='%d', delimiter=' ', header=' '.join(head_name))
 
 
 def load_rl_agent(dataset, filename, epoch_user):
@@ -138,6 +127,7 @@ def load_rl_agent(dataset, filename, epoch_user):
     model_dict = torch.load(model_file)
     print('RL policy model load at {}'.format(model_file))
     return model_dict
+
 
 def save_rl_agent(dataset, model, filename, epoch_user):
     model_file = TMP_DIR[dataset] + '/RL-agent/' + filename + '-epoch-{}.pkl'.format(epoch_user)
@@ -147,7 +137,6 @@ def save_rl_agent(dataset, model, filename, epoch_user):
     print('RL policy model saved at {}'.format(model_file))
 
 
-
 def save_rl_mtric(dataset, filename, epoch, SR, spend_time, mode='train'):
     PATH = TMP_DIR[dataset] + '/RL-log-merge/' + filename + '.txt'
     if not os.path.isdir(TMP_DIR[dataset] + '/RL-log-merge/'):
@@ -155,7 +144,7 @@ def save_rl_mtric(dataset, filename, epoch, SR, spend_time, mode='train'):
     if mode == 'train':
         with open(PATH, 'a') as f:
             f.write('===========Train===============\n')
-            f.write('Starting {} user tuples\n'.format(epoch))
+            f.write('Starting {} user epochs\n'.format(epoch))
             f.write('training SR@5: {}\n'.format(SR[0]))
             f.write('training SR@10: {}\n'.format(SR[1]))
             f.write('training SR@15: {}\n'.format(SR[2]))
@@ -175,6 +164,7 @@ def save_rl_mtric(dataset, filename, epoch, SR, spend_time, mode='train'):
             f.write('================================\n')
             # f.write('1000 loss: {}\n'.format(loss_1000))
 
+
 def save_rl_model_log(dataset, filename, epoch, epoch_loss, train_len):
     PATH = TMP_DIR[dataset] + '/RL-log-merge/' + filename + '.txt'
     if not os.path.isdir(TMP_DIR[dataset] + '/RL-log-merge/'):
@@ -183,6 +173,23 @@ def save_rl_model_log(dataset, filename, epoch, epoch_loss, train_len):
         f.write('Starting {} epoch\n'.format(epoch))
         f.write('training loss : {}\n'.format(epoch_loss / train_len))
         # f.write('1000 loss: {}\n'.format(loss_1000))
+
+
+def save_pretrain_data(dataset, data):
+    path = TMP_DIR[dataset] + '/Pretrain-data/' + '{}-pretrain-data.pkl'.format(dataset)
+    if not os.path.isdir(TMP_DIR[dataset] + '/Pretrain-data/'):
+        os.makedirs(TMP_DIR[dataset] + '/Pretrain-data/')
+    with open(path, 'wb') as f:
+        pickle.dump(data, f)
+        print('{} Pretrain_data saved successfully!'.format(dataset))
+
+
+def load_pretrain_data(dataset):
+    path = TMP_DIR[dataset] + '/Pretrain-data/' + '{}-pretrain-data.pkl'.format(dataset)
+    with open(path, 'rb') as f:
+        data = pickle.load(f)
+        print('{} Pretrain_data load successfully!'.format(dataset))
+        return data
 
 
 def set_random_seed(seed):
@@ -196,6 +203,7 @@ def set_random_seed(seed):
 # Disable
 def blockPrint():
     sys.stdout = open(os.devnull, 'w')
+
 
 # Restore
 def enablePrint():
